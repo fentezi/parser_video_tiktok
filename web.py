1d1968f5c344781ec3452c9405cc382d4f49c534
fentezi <serouhn@gmail.com>
2023-11-07 16:25:08 +0200

    browser open

--- web.py
+++ web.py
@@ -27,68 +27,67 @@ async def kill_all_chrome_processes():
             print(f"No permission to terminate process {proc.pid}")
 
 
-async def process_video(number_pc: int) -> dict:
-    try:
-        with open("accounts.txt", "r", encoding="utf-8") as file:
-            lines = file.read().splitlines()
-        file_fail = open('accounts_fail.txt', 'w', encoding="utf-8")
-        file_successful = open('account_successful.txt', 'w', encoding="utf-8")
-        session_ids = dict()
-        for line in lines:
-            await asyncio.sleep(2)
-            username, password = line.strip().split(';')
-            options = uc.ChromeOptions()
-            options.add_argument("--no-sandbox")
-            options.add_argument("--incognito")
-            options.add_argument("--detach")
-            options.add_argument("--mute-audio")
-            driver = uc.Chrome(use_subprocess=False, options=options, headless=True,
-                               executable_path=ChromeDriverManager().install())
-            stealth(
-                driver,
+async def process_video(number1: int, number2: int, number_pc: int, count_video: int):
+    with open("accounts.txt", "r", encoding="utf-8") as file:
+        lines = file.read().splitlines()
+    session_ids = dict()
+    for line in lines:
+        await asyncio.sleep(2)
+        username, password = line.strip().split(';')
+        options = uc.ChromeOptions()
+        options.add_argument("--incognito")
+        options.add_argument("--mute-audio")
+        driver = uc.Chrome(options=options, headless=True,
+                           executable_path=ChromeDriverManager().install())
+        driver.maximize_window()
+        stealth(driver,
                 languages=["en-US", "en"],
                 vendor="Google Inc.",
-                platform="Win64",
+                platform="Win32",
                 webgl_vendor="Intel Inc.",
                 renderer="Intel Iris OpenGL Engine",
                 fix_hairline=True,
-            )
-            driver.maximize_window()
-            driver.get("https://www.tiktok.com/login/phone-or-email/email")
-            try:
-                logging.info("Browser open")
-                session_id = await login(driver=driver,
-                                         username=username,
-                                         password=password)
-            except TypeError as e:
-                logging.error(e)
-                driver.quit()
-                file_fail.write(f'{username};{password}\n')
+                )
+        driver.get("https://www.tiktok.com/login/phone-or-email/email")
+        try:
+            logging.info("Browser open")
+            session_id = await login(driver=driver,
+                                     username=username,
+                                     password=password)
+        except TypeError as e:
+            logging.error(e)
+            driver.quit()
 
+        else:
+            bot.send_message(chat_id="641487267",
+                             text=f"#{number_pc}. Пользователь {username} авторизован!")
+            session_ids[username] = session_id
+            sleep_time = random.randint(number1, number2) * 60
+            if len(os.listdir('video')) == 1:
+                await posting_video(session_id=session_id,
+                                    bot=bot,
+                                    sleep_time=0,
+                                    username=username,
+                                    number_pc=number_pc,
+                                    count_publish=count_video)
+                break
             else:
-                session_ids[username] = session_id
-                bot.send_message(chat_id="641487267",
-                                 text=f"#{number_pc}. Пользователь {username} авторизован!")
-                file_successful.write(f'{username};{password}\n')
-        file_fail.close()
-        file_successful.close()
-        return session_ids
-    except Exception as e:
-        print(e)
-
-
+                await posting_video(session_id=session_id,
+                                    bot=bot,
+                                    sleep_time=sleep_time,
+                                    username=username,
+                                    number_pc=number_pc,
+                                    count_publish=count_video)
+                await asyncio.sleep(sleep_time)
+    return session_ids
 
 
 async def get_video_process(number1: int, number2: int, number_pc: int, count_video: int):
     print("Запуск загрузчика видео")
-    session_ids = await process_video(number_pc)
-    bot.send_message(chat_id='641487267', text=f'#{number_pc}. Пользователей авторизовано: {len(session_ids)}')
-    while True:
-        if len(os.listdir('video')) == 0:
-            bot.send_message(chat_id='641487267', text=f'#{number_pc}. Папка с видео пустая.')
-            break
+    session_ids = await process_video(number1, number2, number_pc, count_video)
+    while len(os.listdir('video')) != 0:
         for username, id in session_ids.items():
-            sleep_time = random.uniform(number1 * 60, number2 * 60)
+            sleep_time = random.randint(number1, number2) * 60
             if len(os.listdir('video')) == 1:
                 await posting_video(session_id=id,
                                     bot=bot,
@@ -105,8 +104,9 @@ async def get_video_process(number1: int, number2: int, number_pc: int, count_vi
                                     number_pc=number_pc,
                                     count_publish=count_video)
                 await asyncio.sleep(sleep_time)
-
-    await kill_all_chrome_processes()
+    else:
+        bot.send_message(chat_id="641487267", text="Папка с видео пуста")
+        await kill_all_chrome_processes()
 
 
 # async def process_uploaded_file(file: UploadFile,
@@ -147,24 +147,20 @@ async def get_video_process(number1: int, number2: int, number_pc: int, count_vi
 #         return RedirectResponse(error_route_url, status_code=302)
 
 async def start_script():
-    try:
-        script = int(input("Введите:\n1 для запуска парсера видео\n2 для запуска загрузчика видео: \n"))
-        if script == 1:
-            number = int(input("Введите количество видео для парсинга: "))
-            # await main.info_videos(number=number)
-        elif script == 2:
-            number_pc = int(input("Введите номер ПК: "))
-            count_video = int(input("Количество повторных публикаций: "))
-            print("Введите диапазон времени для публикации (в минутах)")
-            number1 = int(input("От (в минутах): "))
-            number2 = int(input("До (в минутах): "))
-            await get_video_process(number1, number2, number_pc, count_video)
-        else:
-            print("Неверный ввод")
-            await start_script()
-    except Exception as e:
-        print(e)
+    script = int(input("Введите:\n1 для запуска парсера видео\n2 для запуска загрузчика видео: \n"))
+    if script == 1:
+        number = int(input("Введите количество видео для парсинга: "))
+        # await main.info_videos(number=number)
+    elif script == 2:
+        number_pc = int(input("Введите номер ПК: "))
+        count_video = int(input("Количество повторных публикаций: "))
+        print("Введите диапазон времени для публикации (в минутах)")
+        number1 = int(input("От (в минутах): "))
+        number2 = int(input("До (в минутах): "))
+        await get_video_process(number1, number2, number_pc, count_video)
+    else:
+        print("Неверный ввод")
+        await start_script()
 
 
-if __name__ == "__main__":
-    asyncio.run(start_script())
+asyncio.run(start_script())
